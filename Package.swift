88b97e8aed7948c7f535d52badaff693b2bcb2a2
Matteo Piccina <altermatte@gmail.com>
2017-10-30 09:41:32 +0100

    Update SwiftyJSON dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     targets: [Target(name: "TelegramBotSDK")],
     dependencies: [
         .Package(url: "https://github.com/zmeyc/CCurl.git", majorVersion: 0),
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15),
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 17),
         .Package(url: "https://github.com/serhii-londar/ScannerUtils.git", majorVersion: 1)
     ]
 )
